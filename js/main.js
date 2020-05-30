4dbdf6680b8a8c8f0ae8cc5dae0ba587c44bfd8e
Marcin <marcin.adam.mroczko@gmail.com>
2020-05-30 18:47:31 +0200

    show eements function

--- js/main.js
+++ js/main.js
@@ -8,11 +8,11 @@ import Test from "./modules/Test"
 // import UserAgent from './modules/UserAgent';
 import Menu from "./modules/Menu"
 import Fix100vh from "./modules/Fix100vh"
-// import AppearElements from "./modules/AppearElements"
+import AppearElements from "./modules/AppearElements"
 
 // Instantiate a new object using our modules/classes
 var test = new Test()
 // var userAgent = new UserAgent();
 var menu = new Menu()
 var fix100vh = new Fix100vh()
-// var appearElements = new AppearElements()
+var appearElements = new AppearElements()
--- js/modules/AppearElements.js
+++ js/modules/AppearElements.js
@@ -1,33 +1,27 @@
 class AppearElements {
   constructor() {
-    // this.vh = window.innerHeight * 0.01
+    this.preloader = document.querySelector(".preloader")
+    this.tlShowElements = new TimelineMax({})
+
     this.events()
   }
 
   events() {
-    window.addEventListener("load", this.showElements.bind(this))
-  }
-
-  showElements() {
-    console.log("show")
-
-    document.addEventListener("DOMContentLoaded", function (event) {
+    document.addEventListener("DOMContentLoaded", event => {
       // wait until window is loaded - all images, styles-sheets, fonts, links, and other media assets
-      window.onload = function () {
+      window.onload = () => {
         // OPTIONAL - waits til next tick render to run code (prevents running in the middle of render tick)
-        window.requestAnimationFrame(function () {
+        window.requestAnimationFrame(() => {
           // GSAP custom code goes here
 
-          document.querySelector(".preloader").classList.add("is-hidden")
+          this.preloader.classList.add("is-hidden")
 
-          tl = new TimelineMax({})
-
-          tl.staggerFrom(".hero__slogan-hidden", 1.5, { y: "100%", opacity: 0, ease: Power4.easeOut }, 0.1)
-          tl.staggerFrom(".tiles__single", 1.5, { y: "15%", opacity: 0, ease: Power4.easeOut }, 0.2, "-=1.5")
-          tl.from(".hero__desc", 0.75, { opacity: 0, ease: Sine.easeOut }, "-=1.5")
-          tl.staggerFrom(".cover", 1.5, { x: "-200%", ease: Power4.easeOut }, 0.2, "-=1.5")
-          tl.from(".btn__inner", 0.1, { visibility: "hidden", ease: Power4.easeOut }, "-=1.4")
-          tl.from(".btn svg rect", 1.5, { opacity: 0, ease: Power4.easeOut }, "-=1")
+          this.tlShowElements.staggerFrom(".hero__slogan-hidden", 1.5, { y: "100%", opacity: 0, ease: Power4.easeOut }, 0.1)
+          this.tlShowElements.staggerFrom(".tiles__single", 1.5, { y: "15%", opacity: 0, ease: Power4.easeOut }, 0.2, "-=1.5")
+          this.tlShowElements.from(".hero__desc", 0.75, { opacity: 0, ease: Sine.easeOut }, "-=1.5")
+          this.tlShowElements.staggerFrom(".cover", 1.5, { x: "-200%", ease: Power4.easeOut }, 0.2, "-=1.5")
+          this.tlShowElements.from(".btn__inner", 0.1, { visibility: "hidden", ease: Power4.easeOut }, "-=1.4")
+          this.tlShowElements.from(".btn svg rect", 1.5, { opacity: 0, ease: Power4.easeOut }, "-=1")
         })
       }
     })
