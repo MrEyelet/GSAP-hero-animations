34695d8f3a5c1868ea918a70c550af664f6b8b30
Marcin <marcin.adam.mroczko@gmail.com>
2020-05-31 17:25:28 +0200

    prefixes

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
-const gulp = require('gulp');
-const autoprefixer = require('gulp-autoprefixer');
+const gulp = require("gulp")
+const autoprefixer = require("gulp-autoprefixer")
 
-gulp.task('prefix', () =>
-    gulp.src('css/style.min.css')
-        .pipe(autoprefixer({
-            browsers: ['last 99 versions'],
-            cascade: false
-    }))
-    .pipe(gulp.dest('css/style.min2.css'))
-);
+gulp.task("prefix", () =>
+  gulp
+    .src("css/style.min.css")
+    .pipe(
+      autoprefixer({
+        browsers: ["last 99 versions"],
+        cascade: false
+      })
+    )
+    .pipe(gulp.dest("css"))
+)
